ab2d795d06b11ff2ca2ee616fc28c2f52fef1e5d
billy <billy3online@hotmail.com>
2025-04-16 19:13:26 +0800

    Merge remote-tracking branch 'origin/master'

--- JavaBasic/src/oop/Demo01.java
+++ JavaBasic/src/oop/Demo01.java
@@ -1,11 +1,14 @@
 package oop;
 
 public class Demo01 {
+
     public static void main(String[] args) {
-        Student student=new Student();
-           student.say();
-
-
+        //调用静态方法
+        Student.say();
+        //调用非静态方法   实例化这个类 new  对象类型   对象名=对象值
+        final Student student = new Student();
+        student.says();
 
     }
+
 }
---
+++ JavaBasic/src/oop/parameter.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class parameter {
+    public static void main(String[] args) {
+        int add= add(4,5);
+        System.out.println("sum"+add);
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
